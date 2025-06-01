4fb17805565487933a4cbba46cd9fd72f6882b89
notaphplover <roberto.pintos.lopez@gmail.com>
2025-06-01 18:52:06 +0200

    feat(reflect-metadata-utils): update getReflectMetadata with propertyKey

--- packages/foundation/libraries/reflect-metadata-utils/src/reflectMetadata/utils/getReflectMetadata.spec.ts
+++ packages/foundation/libraries/reflect-metadata-utils/src/reflectMetadata/utils/getReflectMetadata.spec.ts
@@ -1,45 +1,120 @@
-import { beforeAll, describe, expect, it } from 'vitest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
 import 'reflect-metadata';
 
 import { getReflectMetadata } from './getReflectMetadata';
 
 describe(getReflectMetadata.name, () => {
-  describe('when called, and no metadata is registered', () => {
-    let result: unknown;
+  describe('having no property key', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+    let targetFixture: Function;
+    let metadataKeyFixture: unknown;
 
     beforeAll(() => {
-      result = getReflectMetadata(class {}, 'sample-key');
+      targetFixture = class {};
+      metadataKeyFixture = 'sample-key';
     });
 
-    it('should return undefined', () => {
-      expect(result).toBeUndefined();
+    describe('when called, and no metadata is registered', () => {
+      let result: unknown;
+
+      beforeAll(() => {
+        result = getReflectMetadata(targetFixture, metadataKeyFixture);
+      });
+
+      it('should return undefined', () => {
+        expect(result).toBeUndefined();
+      });
+    });
+
+    describe('when called, and metadata is registered', () => {
+      let result: unknown;
+
+      let metadataFixture: unknown;
+
+      beforeAll(() => {
+        metadataFixture = 'sample-metadata';
+
+        Reflect.defineMetadata(
+          metadataKeyFixture,
+          metadataFixture,
+          targetFixture,
+        );
+
+        result = getReflectMetadata(targetFixture, metadataKeyFixture);
+      });
+
+      afterAll(() => {
+        Reflect.deleteMetadata(metadataKeyFixture, targetFixture);
+      });
+
+      it('should return metadata', () => {
+        expect(result).toBe(metadataFixture);
+      });
     });
   });
 
-  describe('when called, and metadata is registered', () => {
-    let result: unknown;
-
-    let metadataFixture: unknown;
+  describe('having property key', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+    let targetFixture: Function;
+    let metadataKeyFixture: unknown;
+    let propertyKeyFixture: string | symbol;
 
     beforeAll(() => {
-      metadataFixture = 'sample-metadata';
-
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-      const targetFixture: Function = class {};
-      const metadataKeyFixture: unknown = 'sample-key';
-
-      Reflect.defineMetadata(
-        metadataKeyFixture,
-        metadataFixture,
-        targetFixture,
-      );
-
-      result = getReflectMetadata(targetFixture, metadataKeyFixture);
+      targetFixture = class {};
+      metadataKeyFixture = 'sample-key';
+      propertyKeyFixture = Symbol();
     });
 
-    it('should return metadata', () => {
-      expect(result).toBe(metadataFixture);
+    describe('when called, and no metadata is registered', () => {
+      let result: unknown;
+
+      beforeAll(() => {
+        result = getReflectMetadata(
+          targetFixture,
+          metadataKeyFixture,
+          propertyKeyFixture,
+        );
+      });
+
+      it('should return undefined', () => {
+        expect(result).toBeUndefined();
+      });
+    });
+
+    describe('when called, and metadata is registered', () => {
+      let result: unknown;
+
+      let metadataFixture: unknown;
+
+      beforeAll(() => {
+        metadataFixture = 'sample-metadata';
+
+        Reflect.defineMetadata(
+          metadataKeyFixture,
+          metadataFixture,
+          targetFixture,
+          propertyKeyFixture,
+        );
+
+        result = getReflectMetadata(
+          targetFixture,
+          metadataKeyFixture,
+          propertyKeyFixture,
+        );
+      });
+
+      afterAll(() => {
+        Reflect.deleteMetadata(
+          metadataKeyFixture,
+          targetFixture,
+          propertyKeyFixture,
+        );
+      });
+
+      it('should return metadata', () => {
+        expect(result).toBe(metadataFixture);
+      });
     });
   });
 });
