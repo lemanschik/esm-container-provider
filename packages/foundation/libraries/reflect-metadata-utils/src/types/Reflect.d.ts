74ffdb52c3352f0e7cafa614ca7379ce3b441379
notaphplover <roberto.pintos.lopez@gmail.com>
2025-06-01 18:53:20 +0200

    refactor(reflect-metadata-utils): update Reflect internal typings with optional property key

--- packages/foundation/libraries/reflect-metadata-utils/src/types/Reflect.d.ts
+++ packages/foundation/libraries/reflect-metadata-utils/src/types/Reflect.d.ts
@@ -1,1 +1,24 @@
 import 'reflect-metadata';
+
+declare global {
+  namespace Reflect {
+    function getMetadata(
+      metadataKey: unknown,
+      target: object,
+      propertyKey?: string | symbol,
+    ): unknown;
+
+    function getOwnMetadata(
+      metadataKey: unknown,
+      target: object,
+      propertyKey?: string | symbol,
+    ): unknown;
+
+    function defineMetadata(
+      metadataKey: unknown,
+      metadataValue: unknown,
+      target: object,
+      propertyKey?: string | symbol,
+    ): void;
+  }
+}
