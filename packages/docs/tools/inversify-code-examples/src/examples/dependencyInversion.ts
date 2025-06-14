42e66966fb2d4e006fe75edaf8ac40f2df798b01
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-06-14 11:02:32 +0200

    refactor(inversify-code-examples): update dependencyInversion examples
    update examples to rely on ServiceIdentifier type

--- packages/docs/tools/inversify-code-examples/src/examples/dependencyInversion.ts
+++ packages/docs/tools/inversify-code-examples/src/examples/dependencyInversion.ts
@@ -1,11 +1,15 @@
 // Is-inversify-import-example
-import { Container, inject, injectable } from 'inversify';
+import { Container, inject, injectable, interfaces } from 'inversify';
 
 interface Weapon {
   damage: number;
 }
 
-const weaponServiceId: symbol = Symbol.for('WeaponServiceId');
+const ninjaServiceId: interfaces.ServiceIdentifier<Ninja> =
+  Symbol.for('NinjaServiceId');
+
+const weaponServiceId: interfaces.ServiceIdentifier<Weapon> =
+  Symbol.for('WeaponServiceId');
 
 @injectable()
 class Katana {
@@ -22,10 +26,10 @@ class Ninja {
 
 const container: Container = new Container();
 
-container.bind(Ninja).toSelf();
+container.bind(ninjaServiceId).to(Ninja);
 container.bind(weaponServiceId).to(Katana);
 
-const ninja: Ninja = container.get(Ninja);
+const ninja: Ninja = container.get(ninjaServiceId);
 
 console.log(ninja.weapon.damage); // Prints 10
 // End-example
